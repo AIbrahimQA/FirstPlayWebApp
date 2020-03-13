4ed40a073ddb092d0e3edf3ad30d14c62ae7abae
ahmed <ahmed.ibrahim@academytrainee.com>
2020-03-13 14:39:06 +0000

    Play

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,13 +1,15 @@
 package controllers
 
+
 import javax.inject._
 import play.api.mvc._
+
 
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def index = Action {
-    Ok(views.html.index("Hello World"))
+  def index() = Action {
+    Ok("Hello ")
   }
 
 
@@ -19,5 +21,10 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
   def getGreeting1() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index("Hello World! Again again"))
   }
+
+  def google() = Action {
+    Redirect("https://www.google.co.uk/")
+  }
+
 
 }
